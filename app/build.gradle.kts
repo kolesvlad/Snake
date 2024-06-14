7df94400447ea9cd45fcd929b657e88ac625bc05
valdemar <kolesvlad@gmail.com>
2024-06-14 15:53:21 +0300

    Add apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,6 @@ android {
 
 dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
 }
